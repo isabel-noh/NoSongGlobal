748c21e8d8a0f0a754462b615354fa1a788a1b59
pearl313 <ddelsen3130@gmail.com>
2022-11-19 15:21:59 +0900

    영화 & 장르 데이터 저장

--- NPM/BackEnd/movies/models.py
+++ NPM/BackEnd/movies/models.py
@@ -2,17 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-# class Movie(models.Model):
-#     title = models.CharField(max_length=100)
-#     release_date = models.DateField()
-#     genres = models.CharField(max_length=10)
-#     popularity = models.FloatField()
-#     vote_count = models.IntegerField()
-#     vote_average = models.FloatField()
-#     overview = models.TextField()
-#     poster_path = models.CharField(max_length=200)
-
-
 
 class Genre(models.Model):
     # genre_id = models.IntegerField(primary_key=True)
