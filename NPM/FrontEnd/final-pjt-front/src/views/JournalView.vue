ac01c1820789f14c68dae75b3c8ad265623b774f
pearl313 <ddelsen3130@gmail.com>
2022-11-23 21:58:59 +0900

    카드 뒤집기 + 해당 포스터 이미지 보여주기

--- NPM/FrontEnd/final-pjt-front/src/views/JournalView.vue
+++ NPM/FrontEnd/final-pjt-front/src/views/JournalView.vue
@@ -50,15 +50,22 @@
                   </small>
                 </div>
               </div>
-              <div class="item back"
-                v-if="!journal?.journal_image"
-                    style="width: 100%; height: 100px; padding: 10px;">
-              뒷면
+              <div class="item back" v-if="journal?.journal_image">
+                <img
+                  
+                  :src= "journal?.journal_image"
+                  class="card-img-top item back">
+                  
               </div>
-              <img
-                v-if="journal?.journal_image"
-                :src= "journal?.journal_image"
-                class="card-img-top item back" style="padding:10px;">
+              <div 
+                class="item back" 
+                style="margin-bottom:10px;"
+                v-if="journal?.isActive_url">
+                <img 
+                  class="card-img-top item back"
+                  :src="`https://image.tmdb.org/t/p/w500/${journal?.poster_path}`">
+
+              </div>
             </div>
           </div>
         </div>
@@ -75,6 +82,8 @@ export default {
     data(){
       return{
         journalList: [],
+        each_movie: null,
+        isActive_url: null,
       }
     },
     methods: {
@@ -88,10 +97,16 @@ export default {
         })
         .then((response) => {
           for (const j of response.data){
-            console.log(j)
             if(j.journal_image !== null){
               j.journal_image = 'http://localhost:8000' + j.journal_image
+              this.isActive_url = false
+            } else {
+              this.each_movie = j
+              console.log(this.each_movie)
+              // this.poster_url = 'https://image.tmdb.org/t/p/w500' + j.poster_path
+              this.isActive_url = true
             }
+            j['isActive_url'] = this.isActive_url
             j['movieTitle'] = this.movieList[j.movie_id-1].title
             this.journalList.push(j)
           }
@@ -133,21 +148,12 @@ export default {
 }
 
 .container {
-  /* width: 150px;
-  height: 180px; */
+  width: 15rem;
+  height: 15rem;
   /*        부모의 자식 요소가 3차원의 애니메이션 효과를 가질때, 300px의 거리에서 보는 원근감을 줌*/
   perspective: 300px;
 }
 
-/* .container.item {
-  width: 150px;
-  height: 180px;
-  border: 1px solid #828282;
-  font-size: 35px;
-  /*카드의 뒷면을 안보이게 처리-카드가 뒤집히면 뒷면이 안보임*/
-  /* backface-visibility: hidden;
-  transition: 1s; */
-/* } */
 
 .container .item.front {
   /* 앞면 카드가 부유하게 되어, 뒷면 카드가 아래에서 위로 올라감 -> 요소 두개가 겹치게 됨*/
@@ -165,8 +171,9 @@ export default {
 .container .item.back {
   /*y축을 중심으로 -180도 회전*/
   transform: rotateY(-180deg);
-  border: 1px solid #828282;
-  font-size: 35px;
+  height: 15rem;
+  width: 15rem;
+  padding: 2px;
   /*카드의 뒷면을 안보이게 처리-카드가 뒤집히면 뒷면이 안보임*/
   backface-visibility: hidden;
   transition: 1s;
@@ -174,7 +181,21 @@ export default {
 
 .container:hover .item.back {
   transform: rotateY(0deg);
+  /* backface-visibility: hidden;
+  transition: 1s; */
 }
 
+.card-img-top {
+  height: 15rem;
+  width: 15rem;
+  object-fit: cover;
+}
+
+.card{
+  height: 15rem;
+  width: 15rem;
+}
+
+/* v-if="journal?.journal_image" */
 
 </style>
