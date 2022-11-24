c6d767ebf220232ee828d56aa6dc340db8821665
junhong625 <83000975+junhong625@users.noreply.github.com>
2022-11-24 22:34:43 +0900

    []

--- NPM/BackEnd/journals/models.py
+++ NPM/BackEnd/journals/models.py
@@ -11,13 +11,13 @@ class Journal(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, related_name='reviews', on_delete=models.CASCADE)
+    movie       = models.ForeignKey(Movie, related_name='reviews', on_delete=models.CASCADE)
     journal_image = models.ImageField(upload_to="%Y/%m/%d/", max_length=100, blank=True, null=True)
     watched_at = models.DateField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_journals')
-    rank  = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    rank        = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
 
 
 class Comment(models.Model):
